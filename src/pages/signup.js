723b122a5b1804b9586a21f0481dc90ba61598bc
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-22 16:02:10 -0500

    2 files modified

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import signUp from '../firebase/auth/signup';
 import { useRouter } from 'next/navigation';
+import Head from 'next/head';
 
 function Page() {
   const [email, setEmail] = React.useState('');
@@ -22,34 +23,55 @@ function Page() {
     return router.push('/admin');
   };
   return (
-    <div className="wrapper">
-      <div className="form-wrapper">
-        <h1 className="mt-60 mb-30">Sign up</h1>
-        <form onSubmit={handleForm} className="form">
-          <label htmlFor="email">
-            <p>Email</p>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              type="email"
-              name="email"
-              id="email"
-              placeholder="example@mail.com"
-            />
-          </label>
-          <label htmlFor="password">
-            <p>Password</p>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              type="password"
-              name="password"
-              id="password"
-              placeholder="password"
-            />
-          </label>
-          <button type="submit">Sign up</button>
-        </form>
+    <div>
+      <Head>
+        <title>Sign up</title>
+        <link rel="icon" href="/logo.png" sizes="any" />
+      </Head>
+      <div className="wrapper w-4/5 mx-auto my-8 p-6 shadow-xl bg-slate-100 rounded-lg">
+        <h1 className="text-center text-2xl font-bold m-8">Sign up</h1>
+        <div className="form-wrapper w-1/4 mx-auto">
+          <form onSubmit={handleForm} className="form">
+            <label htmlFor="email">
+              <p className="text-xl font-bold m-2">Email</p>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                type="email"
+                name="email"
+                id="email"
+                placeholder="example@mail.com"
+                className="p-2 rounded-lg"
+              />
+            </label>
+            <label htmlFor="password">
+              <p className="text-xl font-bold m-2">Password</p>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                className="p-2 rounded-lg"
+              />
+            </label>
+            <br />
+            <br />
+            <button
+              type="submit"
+              className="text-black w-1/2 text-center border border-amber-500 hover:bg-amber-500 rounded-lg text-lg p-2 mb-4 font-bold mx-auto"
+            >
+              Sign up
+            </button>
+            <p className="text-center">
+              Already have an account?{' '}
+              <a href="/signin" className="underline text-amber-500">
+                Sign in
+              </a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );
