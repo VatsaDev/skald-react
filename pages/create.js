f5ca84334a08ffb1ec8e823e6084078b9749947c
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-21 23:26:37 -0500

    3 files modified, 1 deleted

--- pages/create.js
+++ pages/create.js
@@ -1,12 +1,9 @@
 import Head from 'next/head';
 import React, { useState } from 'react';
-// Import the Slate editor factory.
-import { createEditor } from 'slate';
+import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.bubble.css';
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react';
-
-import RichEditor from '../components/RichEditor.tsx';
 /*async function query(data) {
         const response = await fetch(
             "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct",
@@ -22,6 +19,7 @@ import RichEditor from '../components/RichEditor.tsx';
         const result = await response.json();
         return result;
     }*/ // replace with bert adjective replace
+
 export default function Home() {
   const [postContent, setPostContent] = useState(
     'Write a fantasy adventure story about Leif the viking going to the the magical waterfall of Odin, with its rushing rapids, deep blue water, and its deafening roars, to get the obsidian idol of Odin, in the style of Brandon Mull. magic, dragons, treasure, myths ...'
@@ -31,10 +29,7 @@ export default function Home() {
     const url =
       'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=AIzaSyDucoz8cp-KDI5_LWXBzbepSc6MN1Ly-Iw';
 
-    var textBody =
-      '{ "prompt": { "text": "As a prolific author:' +
-      data +
-      ', in the markdown format, with markdown bolding on titles, like **this**"} }';
+    var textBody = '{ "prompt": { "text": "As a prolific author:' + data + '';
 
     document.getElementById('naraBtn').disabled = true;
 
@@ -58,13 +53,18 @@ export default function Home() {
     gquery(postContent);
   }
 
-  const [editor] = useState(() => withReact(createEditor()));
-  const initialValue = [
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ];
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ script: 'sub' }, { script: 'super' }],
+      ['blockquote', 'code-block'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['clean'],
+    ],
+  };
+
+  const [REvalue, REsetValue] = useState('');
 
   return (
     <div>
@@ -76,9 +76,14 @@ export default function Home() {
 
       <section>
         <h1 className="text-center text-8xl m-8 font-bold">Create</h1>
-        <Slate editor={editor} initialValue={initialValue}>
-          <Editable className="w-5/6 mx-auto h-96 bg-slate-100 drop-shadow-2xl rounded-lg p-8 m-2" />
-        </Slate>
+        <ReactQuill
+          theme="bubble"
+          modules={modules}
+          className="bg-slate-100 block w-5/6 h-96 block w-5/6 mx-auto drop-shadow-2xl rounded-lg p-2 m-8"
+          placeholder="Write a fantasy adventure story about Leif the viking going to the the magical waterfall of Odin, with its rushing rapids, deep blue water, and its deafening roars, to get the obsidian idol of Odin, in the style of Brandon Mull. magic, dragons, treasure, myths ..."
+          value={REvalue}
+          onChange={(e) => REsetValue(e.target.value, console.log(REvalue))}
+        />
         <textarea
           id="editor"
           className="bg-slate-100 block w-5/6 h-96 mx-auto drop-shadow-2xl rounded-lg p-8 m-2"
