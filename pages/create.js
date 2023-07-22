18f7e291682b7ad64faf9d9e5ab0f80d3d395482
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-22 14:57:35 -0500

    1 file modified

--- pages/create.js
+++ pages/create.js
@@ -1,9 +1,5 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.bubble.css';
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
-const { Quill } = dynamic(() => import('react-quill'), { ssr: false });
+import { useState } from 'react';
 
 /*async function query(data) {
         const response = await fetch(
@@ -22,15 +18,16 @@ const { Quill } = dynamic(() => import('react-quill'), { ssr: false });
     }*/ // replace with bert adjective replace
 
 export default function Home() {
-  // put value from server in useState
-  const [REvalue, REsetValue] = useState('');
-  console.log(REvalue);
+  const [postContent, setPostContent] = useState(
+    'Write a fantasy adventure story about Leif the viking going to the the magical waterfall of Odin, with its rushing rapids, deep blue water, and its deafening roars, to get the obsidian idol of Odin, in the style of Brandon Mull. magic, dragons, treasure, myths ...'
+  );
 
   async function gquery(data) {
     const url =
       'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=AIzaSyDucoz8cp-KDI5_LWXBzbepSc6MN1Ly-Iw';
 
-    var textBody = '{ "prompt": { "text": "As a prolific author:' + data + '';
+    var textBody =
+      '{ "prompt": { "text": "As a prolific author:' + data + '"} }';
 
     document.getElementById('naraBtn').disabled = true;
 
@@ -51,46 +48,25 @@ export default function Home() {
   var btnText = 'narrate';
 
   function narrate() {
-    //Quill.insertText('memes');
-    //gquery(REvalue);
+    gquery(postContent);
   }
 
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, 3] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ script: 'sub' }, { script: 'super' }],
-      ['blockquote', 'code-block'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      ['clean'],
-    ],
-  };
-  /*<textarea
-          id="editor"
-          className="bg-slate-100 block w-5/6 h-96 mx-auto drop-shadow-2xl rounded-lg p-8 m-2"
-          type="text"
-          placeholder="hello"
-          value={postContent}
-          onChange={(e) => setPostContent(e.target.value)}
-        />*/
   return (
     <div>
       <Head>
         <title>Create</title>
         <link rel="icon" href="/logo.png" sizes="any" />
-        <meta charset="utf-8" />
       </Head>
 
       <section>
         <h1 className="text-center text-8xl m-8 font-bold">Create</h1>
-        <ReactQuill
+        <textarea
           id="editor"
-          theme="bubble"
-          modules={modules}
-          className="bg-slate-100 block w-5/6 h-96 block w-5/6 mx-auto drop-shadow-2xl rounded-lg p-2 m-8"
-          placeholder="Write a fantasy adventure story about Leif the viking going to the the magical waterfall of Odin, with its rushing rapids, deep blue water, and its deafening roars, to get the obsidian idol of Odin, in the style of Brandon Mull. magic, dragons, treasure, myths ..."
-          value={REvalue}
-          onChange={REsetValue}
+          className="bg-slate-100 block w-4/5 h-96 mx-auto drop-shadow-lg rounded-lg p-8"
+          type="text"
+          placeholder="your story..."
+          value={postContent}
+          onChange={(e) => setPostContent(e.target.value)}
         />
         <div id="btn-wrapper" className="w-4/5 mx-auto">
           <button
