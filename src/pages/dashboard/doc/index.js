65cfc2acc3440f40dade0dc06cbadf51a412cb8b
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-27 14:39:25 -0500

    1 file modified

--- src/pages/dashboard/doc/index.js
+++ src/pages/dashboard/doc/index.js
@@ -9,6 +9,9 @@ export default function Page() {
   const router = useRouter();
   const isReady = useRef(false);
   const [title, setTitle] = useState();
+  const [postContent, setPostContent] = useState(
+    'Write a fantasy adventure story about Leif the viking going to the the magical waterfall of Odin, with its rushing rapids, deep blue water, and its deafening roars, to get the obsidian idol of Odin, in the style of Brandon Mull. magic, dragons, treasure, myths ...'
+  );
   var text;
 
   if (typeof window !== 'undefined') {
@@ -35,13 +38,28 @@ export default function Page() {
   }
 
   return (
-    <h1
-      id="titleStory"
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      className="text-center text-8xl m-2"
-    >
-      {text}
-    </h1>
+    <div>
+      <h1
+        id="titleStory"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="text-center text-8xl m-2"
+      >
+        {text}
+      </h1>
+      <div className="w-1/2 mx-auto">
+        <textarea
+          id="createEditor"
+          className="bg-slate-100 block w-full h-[80rem] my-8 mx-auto drop-shadow-lg rounded-lg p-8"
+          type="text"
+          placeholder="your story..."
+          value={postContent}
+          onChange={(e) => setPostContent(e.target.value)}
+        />
+        <button className="bg-gradient-to-r from-yellow-500 to-amber-600 py-2 px-6 float-right rounded-lg font-bold">
+          Generate
+        </button>
+      </div>
+    </div>
   );
 }
