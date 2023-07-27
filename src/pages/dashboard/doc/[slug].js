13cc6b14ce376d4f779b979469aed1f41a162146
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2023-07-27 09:09:33 -0500

    1 file modified

--- src/pages/dashboard/doc/[slug].js
+++ src/pages/dashboard/doc/[slug].js
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router';
-import getData from '../../../firebase/firestore/getData';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import firebase_app from '../../../firebase/config';
+import { useRef, useEffect, useInterval } from 'react';
+
 export default function Page() {
+  const db = getFirestore(firebase_app);
   const router = useRouter();
-  console.log(getData('stories', router.query.slug));
-  return <p>Post: {router.query.slug}</p>;
+  const isReady = useRef(false);
+  var text = '';
+
+  console.log(router.query.slug);
+  async function readData() {
+    const docRef = doc(db, 'stories', router.query.slug);
+    if (docRef) {
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log(docSnap.data().author);
+        text = docSnap.data().author;
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log('No such document!');
+      }
+    } else {
+      return;
+    }
+  }
+  readData();
+  return <p>Post: {text}</p>;
 }
